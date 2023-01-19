5a0a5299a7a07c383007a12b0c9e68cec3ab2630
matthieuSD <86837261+matthieuSD@users.noreply.github.com>
2023-01-19 18:53:15 +0100

    nationalité

--- select_random_player.py
+++ select_random_player.py
@@ -32,7 +32,7 @@ def get_results(endpoint_url, query):
     return sparql.query().convert()
 
 def create_player_query(club):
-  start_query = '''SELECT DISTINCT ?item ?itemLabel ?team ?pays_de_nationalité_sportive ?pays_de_nationalité_sportiveLabel ?date_de_naissance WHERE{
+  start_query = '''SELECT DISTINCT ?item ?itemLabel ?team ?pays_de_nationalité_sportive ?pays_de_nationalité_sportiveLabel ?date_de_naissance ?pays_de_citoyenneté ?pays_de_citoyennetéLabel WHERE{
     ?item wdt:P106 wd:Q937857.
     ?item wdt:P569 ?date FILTER (?date > "1995-01-01T00:00:00Z"^^xsd:dateTime) .
     ?item wdt:P54 ?team.'''
@@ -41,6 +41,7 @@ def create_player_query(club):
   end_query =  '''SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
   OPTIONAL { ?item wdt:P1532 ?pays_de_nationalité_sportive. }
   OPTIONAL { ?item wdt:P569 ?date_de_naissance. }
+  OPTIONAL { ?item wdt:P27 ?pays_de_citoyenneté. }
   }
   LIMIT 30'''
   query_player = start_query + filter.format(club) + end_query
@@ -65,16 +66,19 @@ def get_player_id():
   random_club_id = random_club[1]
   query_player = create_player_query(random_club_id)
   result_query_player = get_results(endpoint_url, query_player)
+  
   list_players = list()
   for result in result_query_player["results"]["bindings"]:
        list_players.append(result)
-  #players_id = list_q(result_query_player)
-
+  
   random_player = random.choice(list_players)
   random_player_id = random_player['item']['value'].split("/")[-1]
   random_player_name = random_player['itemLabel']['value']
   random_player_birth = random_player['date_de_naissance']['value']
-  random_player_country = random_player['pays_de_nationalité_sportiveLabel']['value']
-  random_player_country_id = random_player['pays_de_nationalité_sportive']['value'].split("/")[-1]
-
+  try:
+    random_player_country = random_player['pays_de_nationalité_sportiveLabel']['value']
+    random_player_country_id = random_player['pays_de_nationalité_sportive']['value'].split("/")[-1]
+  except:
+    random_player_country = random_player['pays_de_citoyennetéLabel']['value']
+    random_player_country_id = random_player['pays_de_citoyenneté']['value'].split("/")[-1]
   return random_player_id, random_player_name, random_player_birth, random_player_country, random_player_country_id, random_club_id, random_club_name
